33f72b5c68f3154f91c8e2a2422a98f538b1ae43
pure-zero <pure.zero.23@gmail.com>
2022-02-10 13:25:06 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mb8aam-dq4won
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mb8aam-dq4won)
---
+++ src/app/log-list/log-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-list',
+  templateUrl: './log-list.component.html',
+  styleUrls: ['./log-list.component.css'],
+})
+
+export class LogListComponent {}
